635fd8963074532db1675c112b70e814bec6a89c
tabira2382 <tabira2382@gmail.com>
2024-05-02 17:00:41 +0900

    wip login

--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('articles/', include('myarticles.urls')),
 ]
--- myarticles/views.py
+++ myarticles/views.py
@@ -11,7 +11,11 @@ class Articles_listView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         response = requests.get('https://qiita.com/api/v2/items', params={'per_page': 20})
         if response.status_code == 200:
-            context['articles'] = response.json()
+            articles = response.json()
+            for article in articles:
+                # タグデータの抽出と整形
+                article['tag_list'] = ', '.join([tag['name'] for tag in article['tags']])
+            context['articles'] = articles
         else:
             context['articles'] = []
         return context
