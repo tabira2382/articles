5812440a0454c5ffe0830f77ec5be357d3ebc142
tabira2382 <tabira2382@gmail.com>
2024-05-22 17:03:18 +0900

    fix:M3チップ対応

--- articles/settings/base_settings.py
+++ articles/settings/base_settings.py
@@ -77,9 +77,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
 }
 
 CORS_ALLOWED_ORIGINS = [
