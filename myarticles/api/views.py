5297d8059b672ce17ab1e9274b0845a4195e2def
tabira2382 <tabira2382@gmail.com>
2024-05-28 23:55:15 +0900

    feat:likes_count

--- myarticles/api/views.py
+++ myarticles/api/views.py
@@ -62,7 +62,6 @@ class LoginAPI(generics.GenericAPIView):
             "token": token.key
         })
 
-
 # いいね取得
 class LikeListAPI(APIView):
     def get(self, request, format=None):
@@ -72,29 +71,35 @@ class LikeListAPI(APIView):
 
 # Qiitaの記事を取得する関数
 def fetch_qiita_articles():
-    response = requests.get('https://qiita.com/api/v2/items', params={'per_page': 20})
+    response = requests.get('https://qiita.com/api/v2/items', params={'query': 'stocks:>100', 'per_page': 2})
     if response.status_code == 200:
         articles = response.json()
         for article in articles:
             article['tag_list'] = ','.join([tag['name'] for tag in article['tags']])
+            article['qiita_likes_count'] = article['likes_count']  # Qiita APIから直接取得
             article['likes_count'] = Like.objects.filter(article_id=article['id']).count()
             article['image_url'] = get_og_image(article['url'])
         return articles
     return []
 
+
+
 # Zennの記事を取得する関数
 def fetch_zenn_articles():
-    response = requests.get('https://zenn.dev/api/articles', params={'order': 'latest'})
+    response = requests.get('https://zenn.dev/api/trend_articles', params={'order': 'latest', 'per_page': 2})
     if response.status_code == 200:
         articles = response.json()['articles']
         for article in articles:
             tags = article.get('topics', []) or article.get('tags', [])
             article['tag_list'] = ','.join(tags)
+            article['zenn_likes_count'] = article.get('liked_count', 0)  # Zenn APIからlikes_countを取得する場合
             article['likes_count'] = 0  # Zennにはいいねの数がない場合、仮に0に設定
             article['url'] = f"https://zenn.dev{article['path']}"  # 完全なURLにする
             article['image_url'] = get_og_image(article['url'])
         return articles
     return []
+
+
 
 # はてなブックマークのテクノロジー記事を取得する関数
 def fetch_hatena_tech_articles():
@@ -113,14 +118,11 @@ def fetch_hatena_tech_articles():
                 entry_response = requests.get(f'https://b.hatena.ne.jp/entry/jsonlite/?url={link}')
                 if entry_response.status_code == 200:
                     entry_data = entry_response.json()
-                    tags = [bookmark.get('tags', []) for bookmark in entry_data.get('bookmarks', [])]
-                    flat_tags = [tag for sublist in tags for tag in sublist]  # フラットなタグリストに変換
-                    unique_tags = list(set(flat_tags))  # 重複を排除
                     article = {
                         'id': entry_data['eid'],
                         'title': entry_data['title'],
                         'url': entry_data['url'],
-                        'tag_list': ','.join(unique_tags),
+                        'tag_list': 'tech',  # 固定のタグリスト
                         'likes_count': entry_data['count'],
                         'image_url': get_og_image(link)  # OGP画像を取得
                     }
@@ -128,7 +130,8 @@ def fetch_hatena_tech_articles():
             except Exception as e:
                 logger.debug(f"Error fetching entry data for {link}: {e}")
         logger.debug(f"Fetched {len(articles)} articles from Hatena")
-        return articles
+        return articles[:2]
+        # return articles
     return []
 
 # 記事一覧API
